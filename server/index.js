a2d9035227d3278f1d3a16f75a985e04d3fa2fc9
Mitchell Thompson <mitchellth86@gmail.com>
2024-02-02 17:30:43 -0800

    wishlists separated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 require('dotenv').config()
 var qs = require('querystring');
 const { searchForGame } = require('./typesense.js');
+var { getWishlistPage, createWishlist, addGameToWishlist, getWishlists } = require("./wishlist.js");
 
 const app = express()
 
@@ -26,7 +27,7 @@ app.use(passport.session());
 
 // index path renders index.ejs with user data
 app.get('/', async function(req, res){
-  var wishlists = await getWishLists(req)
+  var wishlists = await getWishlists(req)
   // req.session.wishlists = wishlists;
   // req.session.save();
   // console.log(wishlists)
@@ -37,100 +38,10 @@ app.get('/', async function(req, res){
 // login path redirects to index after authenticating with Steam
 app.get('/steam/login', passport.authenticate('steam', {failureRedirect: '/'}), login);
 
-async function getWishLists(req) {
-  // console.log(req);
-  if (req.user) {
-    return await admin.firestore().collection('users').doc(req.user.id).get().then(async (docSnapshot) => {
-      if (docSnapshot.exists) {
-        // req.session.wishlists = docSnapshot.data().wishlists;
-        // req.session.save();
-        var db_wishlists = docSnapshot.data().wishlists;
-        // console.log(db_wishlists)
-        if (db_wishlists.length > 0) {
-          return await admin.firestore().getAll(...db_wishlists).then((wishlists) => {
-          // req.session.wishlists = wishlists;
-          // req.session.save();
-          final_wishlists = [];
-          for (var wishlist of wishlists){
-            // console.log(wishlist.id)
-            var wishlist_data = wishlist.data();
-            wishlist_data['id'] = wishlist.id;
-           final_wishlists.push(wishlist_data); 
-          }
-          // console.log(final_wishlists)
-          return final_wishlists;
-          })
-        } else {
-          return [];
-        }
-      } 
-    });
-  }
-}
 
-app.post('/wishlist/create', function(req, res){
-  // console.log(req);
-  var body = '';
-  req.on('data', function (data) {
-      body += data;
+app.post('/wishlist/create', createWishlist);
 
-      if (body.length > 1e6)
-        req.socket.destroy();
-  });
-
-  req.on('end', function () {
-      var post = qs.parse(body);
-      // console.log(post);
-      var wishlist_name = post['wishlist_name'];
-      console.log('creating a new wishlist: ' + wishlist_name);
-      var new_id = uuidv4();
-
-      // console.log('id: ' + new_id);
-      // console.log('user: ' + req.session.passport.user.id);
-
-      wishdoc = admin.firestore().collection('wishlists').doc(new_id).get().then((wishsnapshot) => {
-        if (wishsnapshot.exists) {
-          console.log("wishlist already exists");
-        } else {
-          admin.firestore().collection('wishlists').doc(new_id).set({
-            editors: {},
-            name: post['wishlist_name'],
-            games: {},
-            owner: admin.firestore().collection('users').doc(req.session.passport.user.id)
-          }).then(() => {
-            // console.log("wishlist created in collection");
-            admin.firestore().collection('users').doc(req.session.passport.user.id).update({
-              wishlists: admin.firestore.FieldValue.arrayUnion(admin.firestore().collection('wishlists').doc(new_id))
-            }).then(() => {
-              // console.log("wishlist added to user");
-              res.redirect('/');
-            });
-          });
-        }
-      })
-  });
-});
-
-app.get('/wishlist/:id', async function(req, res){
-  if (req.user) {
-    var wishlist = await admin.firestore().collection('wishlists').doc(req.params.id).get().then((wishsnapshot) => {
-      if (wishsnapshot.exists) {
-        var data = wishsnapshot.data();
-        data['id'] = req.params.id;
-        if (data.editors[req.user.id] || data.owner.id == req.user.id) {
-          res.render('wishlist', { user: req.user, wishlist: data });
-        } else {
-          res.redirect('/');
-        }
-      } else {
-        console.log(req.user.name + " tried to access a wishlist that doesn't exist");
-        res.redirect('/');
-      }
-    })
-  } else {
-    res.redirect('/');
-  }
-});
+app.get('/wishlist/:id', getWishlistPage);
 
 app.get('/game/search/:query', async function(req, res){
   if (req.user) {
@@ -195,34 +106,7 @@ async function getGameData(appid) {
   })
 }
 
-app.get('/game/add/:wishlist_id/:game_id', async function(req, res){
-  if (req.user) {
-    await admin.firestore().collection('wishlists').doc(req.params.wishlist_id).get().then((wishsnapshot) => {
-      if (wishsnapshot.exists) {
-        // console.log(wishsnapshot.data());
-        var data = wishsnapshot.data();
-
-        if (data.editors[req.user.id] || data.owner.id == req.user.id) {
-          getGameData(req.params.game_id).then((gameData) => {
-            
-            admin.firestore().collection('wishlists').doc(req.params.wishlist_id).update({
-              [`games.${req.params.game_id}`]: gameData[req.params.game_id]['data']['name']
-            }).then(() => {
-              res.redirect('/wishlist/' + req.params.wishlist_id)
-            });
-          })
-        } else {
-          res.redirect('/');
-        }
-      } else {
-        console.log(req.user.name + " tried to add a game to a wishlist that doesn't exist");
-        res.redirect('/');
-      }
-    })
-  } else {
-    res.redirect('/');
-  }
-});
+app.get('/game/add/:wishlist_id/:game_id', addGameToWishlist);
 
 // login return path (specified by returnURL in passport's SteamStrategy) redirects to index after authenticating with steam
 // app.get('/steam/login/return', passport.authenticate('steam', {failureRedirect: '/'}), function(req, res){
---
+++ server/wishlist.js
@@ -0,0 +1,130 @@
+var { admin } = require('./initFirebase');
+
+
+async function getWishlists(req) {
+    // console.log(req);
+    if (req.user) {
+        return await admin.firestore().collection('users').doc(req.user.id).get().then(async (docSnapshot) => {
+            if (docSnapshot.exists) {
+                // req.session.wishlists = docSnapshot.data().wishlists;
+                // req.session.save();
+                var db_wishlists = docSnapshot.data().wishlists;
+                // console.log(db_wishlists)
+                if (db_wishlists.length > 0) {
+                    return await admin.firestore().getAll(...db_wishlists).then((wishlists) => {
+                        // req.session.wishlists = wishlists;
+                        // req.session.save();
+                        final_wishlists = [];
+                        for (var wishlist of wishlists) {
+                            // console.log(wishlist.id)
+                            var wishlist_data = wishlist.data();
+                            wishlist_data['id'] = wishlist.id;
+                            final_wishlists.push(wishlist_data);
+                        }
+                        // console.log(final_wishlists)
+                        return final_wishlists;
+                    })
+                } else {
+                    return [];
+                }
+            }
+        });
+    }
+}
+
+function createWishlist(req, res) {
+    // console.log(req);
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+
+        if (body.length > 1e6)
+            req.socket.destroy();
+    });
+
+    req.on('end', function () {
+        var post = qs.parse(body);
+        // console.log(post);
+        var wishlist_name = post['wishlist_name'];
+        console.log('creating a new wishlist: ' + wishlist_name);
+        var new_id = uuidv4();
+
+        // console.log('id: ' + new_id);
+        // console.log('user: ' + req.session.passport.user.id);
+
+        wishdoc = admin.firestore().collection('wishlists').doc(new_id).get().then((wishsnapshot) => {
+            if (wishsnapshot.exists) {
+                console.log("wishlist already exists");
+            } else {
+                admin.firestore().collection('wishlists').doc(new_id).set({
+                    editors: {},
+                    name: post['wishlist_name'],
+                    games: {},
+                    owner: admin.firestore().collection('users').doc(req.session.passport.user.id)
+                }).then(() => {
+                    // console.log("wishlist created in collection");
+                    admin.firestore().collection('users').doc(req.session.passport.user.id).update({
+                        wishlists: admin.firestore.FieldValue.arrayUnion(admin.firestore().collection('wishlists').doc(new_id))
+                    }).then(() => {
+                        // console.log("wishlist added to user");
+                        res.redirect('/');
+                    });
+                });
+            }
+        })
+    });
+}
+
+async function getWishlistPage(req, res) {
+    if (req.user) {
+        var wishlist = await admin.firestore().collection('wishlists').doc(req.params.id).get().then((wishsnapshot) => {
+            if (wishsnapshot.exists) {
+                var data = wishsnapshot.data();
+                data['id'] = req.params.id;
+                if (data.editors[req.user.id] || data.owner.id == req.user.id) {
+                    res.render('wishlist', { user: req.user, wishlist: data });
+                } else {
+                    res.redirect('/');
+                }
+            } else {
+                console.log(req.user.name + " tried to access a wishlist that doesn't exist");
+                res.redirect('/');
+            }
+        })
+    } else {
+        res.redirect('/');
+    }
+}
+
+async function addGameToWishlist(req, res) {
+    if (req.user) {
+        await admin.firestore().collection('wishlists').doc(req.params.wishlist_id).get().then((wishsnapshot) => {
+            if (wishsnapshot.exists) {
+                // console.log(wishsnapshot.data());
+                var data = wishsnapshot.data();
+
+                if (data.editors[req.user.id] || data.owner.id == req.user.id) {
+                    getGameData(req.params.game_id).then((gameData) => {
+
+                        admin.firestore().collection('wishlists').doc(req.params.wishlist_id).update({
+                            [`games.${req.params.game_id}`]: gameData[req.params.game_id]['data']['name']
+                        }).then(() => {
+                            res.redirect('/wishlist/' + req.params.wishlist_id)
+                        });
+                    })
+                } else {
+                    res.redirect('/');
+                }
+            } else {
+                console.log(req.user.name + " tried to add a game to a wishlist that doesn't exist");
+                res.redirect('/');
+            }
+        })
+    } else {
+        res.redirect('/');
+    }
+}
+exports.getWishlistPage = getWishlistPage;
+exports.getWishlists = getWishlists;
+exports.createWishlist = createWishlist;
+exports.addGameToWishlist = addGameToWishlist;
