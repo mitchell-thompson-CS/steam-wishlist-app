01db9190c6a23bc3f76e8bfc3fee5b8b39312c74
Mitchell Thompson <mitchellth86@gmail.com>
2024-02-02 16:26:00 -0800

    refactored and added router

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Wishlist from './component/wishlist';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
-          <Wishlist />
+          <Routes>
+            <Route path="/" element={<Wishlist />} />
+          </Routes>
         </header>
       </div>
     </Provider>
