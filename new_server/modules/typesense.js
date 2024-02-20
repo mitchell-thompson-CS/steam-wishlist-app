0b1445ce1e816a6e1fedee3d368bb81847df57d1
Mitchell Thompson <mitchellth86@gmail.com>
2024-02-20 14:57:05 -0800

    added try catch for the typesenseClient itself

--- new_server/modules/typesense.js
+++ new_server/modules/typesense.js
@@ -2,15 +2,21 @@ const Typesense = require('typesense');
 const { Logging, LogLevels } = require('./logging');
 const { default: axios } = require('axios');
 
-let typesenseClient = new Typesense.Client({
-    'nodes': [{
-        'host': 'localhost', // For Typesense Cloud use xxx.a1.typesense.net
-        'port': 8108,      // For Typesense Cloud use 443
-        'protocol': 'http'   // For Typesense Cloud use https
-    }],
-    'apiKey': `${process.env.TYPESENSE_API_KEY}`,
-    'connectionTimeoutSeconds': 2
-});
+let typesenseClient;
+
+try {
+    typesenseClient = new Typesense.Client({
+        'nodes': [{
+            'host': 'localhost', // For Typesense Cloud use xxx.a1.typesense.net
+            'port': 8108,      // For Typesense Cloud use 443
+            'protocol': 'http'   // For Typesense Cloud use https
+        }],
+        'apiKey': `${process.env.TYPESENSE_API_KEY}`,
+        'connectionTimeoutSeconds': 2
+    });
+} catch (e) {
+    Logging.log("typesense", e, LogLevels.ERROR);
+}
 
 const gameSchema = {
     'name': 'games',
