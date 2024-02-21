9b0f73b617c259a113a9fc215fc98daeb528fcb4
whiteja2 <jacobwhite2000@gmail.com>
2024-02-20 17:40:23 -0800

    WishlistSidebar component separated from Wishlist and WishlistInner

--- client/src/component/wishlistInner.js
+++ client/src/component/wishlistInner.js
@@ -4,13 +4,50 @@ import { createWishlist, deleteWishlist } from "../actions/wishlistAction";
 import axios from "axios";
 import Popup from './Popup';
 import '../Wishlist.css';
+import { useParams } from "react-router-dom";
+import WishlistSidebar from "./WishlistSidebar";
 
+
+async function addGameToWishlist(wishlist, game="105600") {
+    try {
+        let res = await axios.post('/api/game/add', {
+            wishlist_id: wishlist,
+            game_id: game
+        });
+        console.log(res);
+    } catch (error) {
+        console.log("error")
+        console.error(error);
+    }
+}
 
 const WishlistInner = () => {
+    const [wishlistItem, setWishlistItem] = useState([]);
+    let { id } = useParams();
+
+    useEffect(() => {
+        fetch('/api/wishlist/' + id, { mode: 'cors', credentials: 'include' })
+            .then(function (response) {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            }).then(function (data) {
+                if (data) {
+                    setWishlistItem(data);
+                    console.log(data);
+                }
+            })
+    }, []);
 
     return (
-        <div>
-            <h1>Wishlist Page</h1>
+        <div className="wishlistInner">
+            <WishlistSidebar />
+            <ul className="gameList">
+                {wishlistItem.games && Object.entries(wishlistItem.games).map(([key, value]) => (
+                    <li key={key} className="gameItem">{value}</li>
+                ))}
+            </ul>
+            <button onClick={() => addGameToWishlist(id)}>Add Game to Wishlist</button>
         </div>
     )
 }
