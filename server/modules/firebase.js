ba677f96dab6e2890972eea97612635a30abe1e9
Mitchell Thompson <mitchellth86@gmail.com>
2024-02-22 17:19:01 -0800

    Fix Firebase admin initialization for test environment compatibility

--- server/modules/firebase.js
+++ server/modules/firebase.js
@@ -1,12 +1,12 @@
 require('dotenv').config({ path: __dirname + '/../../.env' });
 const admin = require("firebase-admin");
-const serviceAccount = require(`../private/${process.env.FIREBASE_ADMIN_CREDENTIALS_FILE_NAME}`);
 
 let firebaseAdmin;
 
 let db;
 
 if (process.env.NODE_ENV !== "test" && process.env.NODE_ENV !== "test-dev") {
+  const serviceAccount = require(`../private/${process.env.FIREBASE_ADMIN_CREDENTIALS_FILE_NAME}`);
   firebaseAdmin = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
