7dbc0585f91bf3d6d1a24ade384bd14dfd5f739c
Mitchell Thompson <mitchellth86@gmail.com>
2024-03-03 01:26:27 -0800

    began working on game content stuff (very unfinished, basically just began)

--- client/src/component/Game.js
+++ client/src/component/Game.js
@@ -1,11 +1,13 @@
 import GameSidebar from "./GameSidebar"
 
 import '../styles/Game.css';
+import GameContent from "./GameContent";
 
 const Game = () => {
     return (
         <div className="game">
             <GameSidebar />
+            <GameContent />
         </div>
     )
 }
---
+++ client/src/component/GameContent.js
@@ -0,0 +1,17 @@
+
+const GameContent = () => {
+    return (
+        <div className="game-content">
+            <div className="game-content-section">
+                <div className="game-content-section-header">
+                    <h2>Game Content</h2>
+                </div>
+                <div className="game-content-section-body">
+                    <p>Game Content</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GameContent;
