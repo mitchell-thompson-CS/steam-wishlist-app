990b1b2fbb3ae07ebeda4de2dac5ed967f3d1faf
Mitchell Thompson <mitchellth86@gmail.com>
2024-03-05 15:49:19 -0800

    search results now show up in the popup

--- client/src/component/SearchForGamePopup.js
+++ client/src/component/SearchForGamePopup.js
@@ -1,13 +1,16 @@
 import { useParams } from 'react-router-dom';
 import '../styles/SearchForGamePopup.css';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchPopup } from '../actions/eventAction';
+
+const searchDelay = 500;
 
 const SearchForGamePopup = (props) => {
 
     const dispatch = useDispatch();
     const searchPopup = useSelector(state => state.eventReducer.searchPopup);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const closePopup = useCallback((e) => {
         if (e.target.className === "search-for-game-popup" || e.key === "Escape") {
@@ -18,7 +21,7 @@ const SearchForGamePopup = (props) => {
     }, [dispatch])
 
     useEffect(() => {
-        if(props.trigger) {
+        if (props.trigger) {
             document.addEventListener('click', closePopup);
             document.addEventListener('keydown', closePopup);
         } else {
@@ -31,16 +34,66 @@ const SearchForGamePopup = (props) => {
         console.log(searchPopup);
     }, [searchPopup]);
 
+    useEffect(() => {
+        // setup delay for the search bar
+        const delayDebounce = setTimeout(() => {
+            if (searchTerm && searchTerm !== "") {
+                fetch('/api/game/search/' + searchTerm, { mode: 'cors', credentials: 'include' })
+                    .then(function (response) {
+                        if (response.status === 200) {
+                            return response.json();
+                        }
+                    }).then(function (data) {
+                        if (data) {
+                            let searchResults = document.getElementById("popup-search-results");
+                            searchResults.innerHTML = "";
+                            for (let i = 0; i < data.length; i++) {
+                                let cur_data = data[i].document;
+                                let a = document.createElement("a");
+                                a.href = "/game/" + cur_data.appid;
+                                a.innerHTML = "<li>" + cur_data.name + "</li>";
+                                a.className = "searchResult";
+                                a.id = "searchResult" + i;
+                                a.onmouseover = function (event) {
+                                    for (let j = 0; j < searchResults.children.length; j++) {
+                                        searchResults.children[j].style.backgroundColor = null;
+                                    }
+                                    event.target.style.backgroundColor = "#282e35";
+                                    // searchPosition.current = i;
+                                };
+                                a.onmouseout = function (event) {
+                                    event.target.style.backgroundColor = null;
+                                }
+                                searchResults.appendChild(a);
+                            }
+
+                        }
+                    });
+            }
+        }, searchDelay);
+
+        // clear the timeout every time the input changes
+        return () => clearTimeout(delayDebounce);
+    }, [searchTerm]);
+
     return (
         props.trigger ?
             <div className="search-for-game-popup">
                 <div className="search-for-game-popup-inner">
                     <h1>Search for a game</h1>
-                    <input type="text" placeholder="Enter a game name" />
-                    <button>Search</button>
+                    <div id="popup-search-area">
+                        <input type="text" id="popup-game-search" placeholder="Enter a game name" autoComplete='off'
+                            onChange={(e) => {
+                                if (e.target.value !== searchTerm) {
+                                    setSearchTerm(e.target.value);
+                                }
+                            }}
+                        />
+                        <ul id="popup-search-results"></ul>
+                    </div>
                 </div>
             </div>
-        : null
+            : null
     );
 }
 
