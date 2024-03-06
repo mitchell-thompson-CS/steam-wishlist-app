d7936bbbac3b754f162dbeb37a722575bbb78a12
Mitchell Thompson <mitchellth86@gmail.com>
2024-03-05 17:54:42 -0800

    got much of the searching working

--- client/src/component/SearchForGamePopup.js
+++ client/src/component/SearchForGamePopup.js
@@ -2,7 +2,9 @@ import { useParams } from 'react-router-dom';
 import '../styles/SearchForGamePopup.css';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchPopup } from '../actions/eventAction';
+import { setAddGameToWishlist, setEvent, setLoading, setSearchPopup } from '../actions/eventAction';
+import axios from 'axios';
+import { addGameToWishlist } from '../actions/wishlistAction';
 
 const searchDelay = 500;
 
@@ -10,10 +12,13 @@ const SearchForGamePopup = (props) => {
 
     const dispatch = useDispatch();
     const searchPopup = useSelector(state => state.eventReducer.searchPopup);
+    const wishlistItems = useSelector(state => state.wishlistReducer.wishlists);
     const [searchTerm, setSearchTerm] = useState("");
 
     const closePopup = useCallback((e) => {
-        if (e.target.className === "search-for-game-popup" || e.key === "Escape") {
+        if (e.target.className === "search-for-game-popup" 
+        || e.target.id === "search-for-game-popup-blur"
+        || e.key === "Escape") {
             dispatch(setSearchPopup(false));
             document.removeEventListener('click', closePopup);
             document.removeEventListener('keydown', closePopup);
@@ -27,11 +32,12 @@ const SearchForGamePopup = (props) => {
         } else {
             document.removeEventListener('click', closePopup);
             document.removeEventListener('keydown', closePopup);
+            setSearchTerm("");
         }
     }, [props.trigger, closePopup]);
 
     useEffect(() => {
-        console.log(searchPopup);
+        // console.log(searchPopup);
     }, [searchPopup]);
 
     useEffect(() => {
@@ -46,27 +52,64 @@ const SearchForGamePopup = (props) => {
                     }).then(function (data) {
                         if (data) {
                             let searchResults = document.getElementById("popup-search-results");
-                            searchResults.innerHTML = "";
-                            for (let i = 0; i < data.length; i++) {
-                                let cur_data = data[i].document;
-                                let a = document.createElement("a");
-                                a.href = "/game/" + cur_data.appid;
-                                a.innerHTML = "<li>" + cur_data.name + "</li>";
-                                a.className = "searchResult";
-                                a.id = "searchResult" + i;
-                                a.onmouseover = function (event) {
-                                    for (let j = 0; j < searchResults.children.length; j++) {
-                                        searchResults.children[j].style.backgroundColor = null;
+                            // setting the search results now that we have data
+                            if (searchResults) {
+                                searchResults.innerHTML = "";
+                                for (let i = 0; i < data.length; i++) {
+                                    if (!data[i] || !data[i].document) {
+                                        continue;
                                     }
-                                    event.target.style.backgroundColor = "#282e35";
-                                    // searchPosition.current = i;
-                                };
-                                a.onmouseout = function (event) {
-                                    event.target.style.backgroundColor = null;
-                                }
-                                searchResults.appendChild(a);
-                            }
+                                    let cur_data = data[i].document;
+                                    let new_el;
+                                    // only when they call this popup trying to add a game to a specific wishlist
+                                    if (searchPopup && searchPopup.addingGame) {
+                                        new_el = document.createElement("p");
+                                        new_el.innerHTML = "<li>" + cur_data.name + "</li>";
+                                        new_el.onclick = async function (event) {
+                                            // add the game to the wishlist then close the popup
+                                            dispatch(setLoading(true));
+                                            try {
+                                                console.log(cur_data.appid, searchPopup.addingGame)
+                                                let res_add = await axios.post('/api/game/add', { game_id: cur_data.appid, wishlists: [searchPopup.addingGame] });
+                                                if (res_add.status === 200) {
+                                                    dispatch(setEvent(true, "Game added to wishlist"));
+                                                    if (wishlistItems && wishlistItems.owned && wishlistItems.owned[searchPopup.addingGame]) {
+                                                        dispatch(addGameToWishlist(searchPopup.addingGame, "owned", cur_data.appid, cur_data.name))
+                                                    } else if (wishlistItems && wishlistItems.shared && wishlistItems.shared[searchPopup.addingGame]) {
+                                                        dispatch(addGameToWishlist(searchPopup.addingGame, "shared", cur_data.appid, cur_data.name))
+                                                    }
+                                                }
+                                            } catch (error) {
+                                                dispatch(setEvent(false, "Error adding game to wishlist"));
+                                            }
+                                            dispatch(setLoading(false));
+                                            dispatch(setSearchPopup(false));
+                                        }
+                                    } else {
+                                        // no wishlist to add to, so just make it a link to the game
+                                        new_el = document.createElement("a");
+                                        new_el.href = "/game/" + cur_data.appid;
+                                        new_el.innerHTML = "<li>" + cur_data.name + "</li>";
+                                    }
 
+                                    if (new_el) {
+                                        new_el.className = "popup-search-result";
+                                        new_el.id = "popup-search-result" + i;
+                                        new_el.title = cur_data.name;
+                                        new_el.onmouseover = function (event) {
+                                            for (let j = 0; j < searchResults.children.length; j++) {
+                                                searchResults.children[j].style.backgroundColor = null;
+                                            }
+                                            event.currentTarget.style.backgroundColor = "#282e35";
+                                            // searchPosition.current = i;
+                                        };
+                                        new_el.onmouseout = function (event) {
+                                            event.target.style.backgroundColor = null;
+                                        }
+                                        searchResults.appendChild(new_el);
+                                    }
+                                }
+                            }
                         }
                     });
             }
@@ -74,23 +117,21 @@ const SearchForGamePopup = (props) => {
 
         // clear the timeout every time the input changes
         return () => clearTimeout(delayDebounce);
-    }, [searchTerm]);
+    }, [searchTerm, searchPopup, dispatch, wishlistItems]);
 
     return (
         props.trigger ?
             <div className="search-for-game-popup">
+                <div id="search-for-game-popup-blur"></div>
                 <div className="search-for-game-popup-inner">
-                    <h1>Search for a game</h1>
-                    <div id="popup-search-area">
-                        <input type="text" id="popup-game-search" placeholder="Enter a game name" autoComplete='off'
-                            onChange={(e) => {
-                                if (e.target.value !== searchTerm) {
-                                    setSearchTerm(e.target.value);
-                                }
-                            }}
-                        />
-                        <ul id="popup-search-results"></ul>
-                    </div>
+                    <input type="text" id="popup-game-search" placeholder="Enter a game name" autoComplete='off'
+                        onChange={(e) => {
+                            if (e.target.value !== searchTerm) {
+                                setSearchTerm(e.target.value);
+                            }
+                        }}
+                    />
+                    <ul id="popup-search-results"></ul>
                 </div>
             </div>
             : null
