c41240854881283be116b12517de3c1f5ec5bb49
Mitchell Thompson <mitchellth86@gmail.com>
2024-04-02 17:48:28 -0700

    added typesense initialization back

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const { app } = require('./modules/server');
+const { startTypesense } = require('./modules/typesense');
 
 app.listen(3001, () => {
+    startTypesense(true);
     console.log("Server started on port 3001");  
 });
