180f9f7d664ebfb86c2d74abe46e9a56692cb204
Mitchell Thompson <mitchellth86@gmail.com>
2024-04-27 17:13:42 -0700

    final touch ups to readme and index

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const { app } = require('./modules/server');
 const { startTypesense } = require('./modules/typesense');
 const schedule = require('node-schedule');
 
+const PORT = 3001;
+
 const typeSenseCB = () => {
     startTypesense(false);
 }
@@ -9,7 +11,7 @@ const typeSenseCB = () => {
 // every day at 10:05 AM, re-initialize the typesense in case new games were added to Steam
 schedule.scheduleJob('0 5 10 * * *', typeSenseCB);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     startTypesense(true);
-    console.log("Server started on port 3001");  
+    console.log("Server started on port " + PORT);  
 });
